25edeeedca7f7fdd4f619ec30d6884f92d85c389
salonimore6125-debug <salonimore6125@gmail.com>
2026-05-27 12:25:49 +0530

    JWT authentication system completed

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { AuthService } from '../services/auth.service';
+
+import { LoginDto } from '../modules/auth/dto/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { AuthController } from '../../controllers/auth.controller';
 import { AuthService } from '../../services/auth.service';
 
 @Module({
+  imports: [
+    ConfigModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+
+      inject: [ConfigService],
+
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+
+        signOptions: {
+          expiresIn: (configService.get<string>('JWT_EXPIRES_IN') ||
+           '1d') as any,
+        },
+      }),
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+
+  providers: [AuthService],
+
+  exports: [AuthService],
 })
 export class AuthModule {}
