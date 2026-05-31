08686e66f13edf1ae883ccfb425bbd65ee65ac2f
salonimore6125-debug <salonimore6125@gmail.com>
2026-06-01 00:32:31 +0530

    Allow production CORS

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-  origin: 'http://localhost:5173',
+  origin: true,
   credentials: true,
 });
   app.setGlobalPrefix('api/v1');
