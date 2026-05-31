792a3903349a24a10ac1e093134ba671df054144
salonimore6125-debug <salonimore6125@gmail.com>
2026-06-01 00:08:08 +0530

    Add CORS support for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+});
   app.setGlobalPrefix('api/v1');
 
   app.useGlobalPipes(
